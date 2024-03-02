71e7bb493f065c882657996ead24686f78fc0cb5
naizegalik <160127019+naizegalik@users.noreply.github.com>
2024-03-02 11:33:48 +0300

    kurulum

--- chiakurulum.sh
+++ chiakurulum.sh
@@ -14,6 +14,7 @@ sudo apt --reinstall install fuse -y
 sudo apt-get install rclone -y
 sudo apt-get install unzip -y
 
+mkdir /root/plt
 
 
 
@@ -39,31 +40,22 @@ cd
 
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 
-mv kurulum/gonder1.sh /root
-mv kurulum/gonder2.sh /root
-mv kurulum/gonder3.sh /root
-mv kurulum/gonder4.sh /root
-screen -dmS gonder1 bash gonder1.sh && screen -dmS gonder2 bash gonder2.sh && screen -dmS gonder3 bash gonder3.sh && screen -dmS gonder4 bash gonder4.sh
-mkdir /root/upload4 && mkdir /root/upload1 && mkdir /root/upload3 && mkdir /root/upload2
-mkdir /root/plot
-cd
-apt install dos2unix
 
-sleep 1
-mv kurulum/move.sh /root
-cd /root
-mv move.sh /root/plot && dos2unix /root/plot/move.sh && cd /root/plot && chmod 777 move.sh && screen -dmS move bash move.sh && cd
+sudo apt-get install -y rand
 
 cd ~
+mv /root/kurulum/x.sh /root
+mv /root/kurulum/automadmax.sh /root
+screen -dmS oto bash -c 'bash automadmax.sh'
 
-mv kurulum/upload.sh /root
-cd /root
-cp -r upload.sh /root/upload1 && cp -r upload.sh /root/upload2 && cp -r upload.sh /root/upload3 && cp -r upload.sh /root/upload4 && chmod 777 /root/upload1/upload.sh && chmod 777 /root/upload2/upload.sh && chmod 777 /root/upload3/upload.sh && chmod 777 /root/upload4/upload.sh && dos2unix /root/upload1/upload.sh && dos2unix /root/upload2/upload.sh && dos2unix /root/upload3/upload.sh && dos2unix /root/upload4/upload.sh && cd /root/upload1 && sleep 1 && screen -dmS upload1 bash upload.sh && cd && cd /root/upload2 && sleep 1 && screen -dmS upload2 bash upload.sh && cd && cd /root/upload3 && sleep 1 && screen -dmS upload3 bash upload.sh && cd && cd /root/upload4 && screen -dmS upload4 bash upload.sh
-cd
-apt install pip -y && apt install screen -y && sudo apt install -y libsodium-dev cmake g++ git && sudo apt update && sudo apt install cmake build-essential libnuma-dev -y
-cd
-mkdir /root/temp
-git clone --recursive https://github.com/Chia-Network/bladebit.git && apt remove cmake -y && pip install cmake --upgrade && cd bladebit && sh build.sh && cd build-release && screen -dmS plot ./bladebit -n 9999 -c xch1m34zpgre2cz406neagtfjxgt6q4sffu37e9txctn2gx3ex0dw66qxzn5lz -f 8d32e1645c12df89131e03484569ff5287e09ed5a39ff5df7fb1c671eb730ae4aef20155d7babb4fd86f1eb5b8719487 ramplot /root/plot
+screen -dmS plot bash -c 'bash x.sh'
+
+mv /root/kurulum/upload.sh /root/plt/
+cd /root/plt
+screen -dmS upload bash -c 'bash upload.sh'
+
+
+cd ~
 
 screen -x plot
 cd ~
